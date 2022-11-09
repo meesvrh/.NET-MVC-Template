c6e1c404417ef561bf92f32ecb4050d7296e10ba
Mees Verhelpen <mees.verhelpen@gmail.com>
2022-11-09 21:06:41 +0100

    Added RepositoryManager with DomainService

---
+++ Infrastructure/Repositories/RepoManager.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Services;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public sealed class RepoManager : IRepoManager
+    {
+        private readonly BusinessContext _context;
+
+        // private IUserRepo _userRepo;
+
+        public RepoManager(BusinessContext context)
+        {
+            _context = context;
+        }
+
+        // public IUserRepo User => _userRepo ??= new UserRepo(_context);
+
+        public void Save() => _context.SaveChanges();
+
+        public async Task SaveAsync() => await _context.SaveChangesAsync();
+    }
+}
