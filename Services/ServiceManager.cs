eb1095775d3993f5143cbed95c580cf33b8b3962
Mees Verhelpen <mees.verhelpen@gmail.com>
2022-11-09 21:09:21 +0100

    Added ServiceManager + Contract

---
+++ Services/ServiceManager.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Services;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public sealed class ServiceManager : IServiceManager
+    {
+        private readonly IRepoManager _repoManager;
+
+        // private IUserService _userService
+
+        public ServiceManager(IRepoManager repoManager)
+        {
+            _repoManager = repoManager;
+        }
+
+        // public IUserService User => _userService ??= new UserService(_repoManager);
+    }
+}
